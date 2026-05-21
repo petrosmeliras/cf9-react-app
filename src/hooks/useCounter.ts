23db511854a03d1350ea80d25fc1484fa948d3e4
P.Meliras <petros.meliras@gmail.com>
2026-05-22 00:21:32 +0300

    feat: using external file for project types

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import type {CounterState} from "../types.ts";
 
 const initialState = {
   count: 0,
@@ -7,7 +8,7 @@ const initialState = {
 }
 
 export const useCounter = () => {
-  const [state, setState] = useState(initialState);
+  const [state, setState] = useState<CounterState>(initialState);
 
   const getCurrentTime = () => new Date().toLocaleTimeString();
 
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type CounterState = {
+  count: number;
+  lastAction: string;
+  time: string;
+}
