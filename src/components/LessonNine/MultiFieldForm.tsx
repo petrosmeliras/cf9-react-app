7ff292fd0f610e6dbde89131a1e708ce3a3cda53
P.Meliras <petros.meliras@gmail.com>
2026-06-01 21:02:21 +0300

    chore: add spacing in submittedData

--- src/components/LessonNine/MultiFieldForm.tsx
+++ src/components/LessonNine/MultiFieldForm.tsx
@@ -79,7 +79,7 @@ const MultiFieldForm = () => {
 
       {submittedData && (
         <>
-          <div className="mt-6 max-w-md mx-auto border-t space-y-2">
+          <div className="mt-6 pt-4 max-w-md mx-auto border-t space-y-2">
             <p><strong>Name:</strong> {submittedData.name}</p>
             <p><strong>Email:</strong> {submittedData.email}</p>
             <p><strong>Message:</strong> {submittedData.message}</p>
