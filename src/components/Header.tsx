794934aab559237d3fa02175501f5668b242c67b
P.Meliras <petros.meliras@gmail.com>
2026-06-06 00:32:38 +0300

    feat(ui): add product link on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,6 +10,7 @@ const Header = () => {
           </Link>
           <nav className="flex gap-4 text-white font-medium">
             <Link to="/">Home</Link>
+            <Link to="/products">Products</Link>
           </nav>
         </div>
       </header>
