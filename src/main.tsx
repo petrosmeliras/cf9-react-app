e7d95d708a6ad7ca0a67410d8d93b2b3b513862e
P.Meliras <petros.meliras@gmail.com>
2026-06-14 21:23:54 +0300

    feat(auth): add AuthProvider to root file

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "./index.css"
 import {BrowserRouter} from "react-router";
+import {AuthProvider} from "@/context/AuthProvider.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>,
   </StrictMode>,
+
 )
